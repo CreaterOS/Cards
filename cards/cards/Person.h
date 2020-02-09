0660670b5b5863935609d0d4e479eae7cfb0ff43
CreaterOS <51728089+CreaterOS@users.noreply.github.com>
2020-02-09 16:18:12 +0800

    create new file with person

---
+++ cards/cards/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  cards
+//
+//  Created by Bryant Reyn on 2020/2/9.
+//  Copyright © 2020 Bryant Reyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ cards/cards/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  cards
+//
+//  Created by Bryant Reyn on 2020/2/9.
+//  Copyright © 2020 Bryant Reyn. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
